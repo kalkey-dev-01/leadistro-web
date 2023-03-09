038299a35add9bfbcee496b335bc686c83167392
Raman Halder <ramanhalder12@gmail.com>
2023-03-09 15:51:34 +0530

    --forked-template

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'leadistro',
+  title: 'Cold mail Designer and Distributor',
+  description: 'Minimalistic Application for all your marketing needs',
+  locale: 'en',
+};
