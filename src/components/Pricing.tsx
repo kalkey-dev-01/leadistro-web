1dab9930295c08c935bc100479d4fe5bf49b14fc
Raman Halder <ramanhalder12@gmail.com>
2023-05-11 13:11:19 +0530

    Update: Pricing Component

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 interface PricingProps {}
-
+// TODO :- UseState Billing Method TO change tags. First need Paddle Or Paypal to support Payments.
+type BillingMethod = 'month' | 'year';
 const Pricing: React.FC<PricingProps> = () => {
+  const [billingMethod, setBillingMethod] =
+    React.useState<BillingMethod>('month');
   return (
     <div className="relative my-10 min-h-screen text-leadistroWhite">
       <div className="flex min-h-screen w-full flex-col items-center justify-center gap-2">
@@ -17,9 +20,33 @@ const Pricing: React.FC<PricingProps> = () => {
           needs. Both tiers offer robust features and powerful tools to help you
           optimize your email marketing campaigns and drive sales.
         </p>
+        <div className="relative mt-6 flex self-center rounded-lg border border-leadistroDark bg-leadistroDarkComp p-0.5 sm:mt-8">
+          <button
+            onClick={() => setBillingMethod('month')}
+            type="button"
+            className={`${
+              billingMethod === 'month'
+                ? 'relative w-1/2 border-leadistroWhite bg-leadistroDark text-white shadow-sm'
+                : 'relative ml-0.5 w-1/2 border border-transparent text-leadistroWhite'
+            } m-1 whitespace-nowrap rounded-md py-2 text-sm font-medium focus:z-10 focus:outline-none focus:ring-2 focus:ring-leadistroGray  sm:w-auto sm:px-8`}
+          >
+            Monthly billing
+          </button>
+          <button
+            onClick={() => setBillingMethod('year')}
+            type="button"
+            className={`${
+              billingMethod === 'year'
+                ? 'relative w-1/2 border-leadistroWhite bg-leadistroDark text-white shadow-sm'
+                : 'relative ml-0.5 w-1/2 border border-transparent text-leadistroWhite'
+            } m-1 whitespace-nowrap rounded-md py-2 text-sm font-medium focus:z-10 focus:outline-none focus:ring-2 focus:ring-leadistroGray  sm:w-auto sm:px-8`}
+          >
+            Yearly billing
+          </button>
+        </div>
         <div className="grid w-full grid-cols-1 gap-2 py-4 md:max-w-screen-md md:grid-cols-2 md:gap-4 lg:max-w-screen-lg lg:gap-5 xl:max-w-screen-xl xl:gap-6">
-          <div className="card-bordered card flex flex-col items-center justify-between rounded-2xl border-2 border-leadistroWhite py-5">
-            <h6 className="text-base md:text-lg lg:text-xl xl:text-2xl">
+          <div className="card-bordered card card-body flex flex-col items-center justify-between rounded-2xl border-2 border-leadistroWhite bg-leadistroDark/30 py-5 font-poppins backdrop-blur-[6px]">
+            <h6 className=" card-title text-base font-extralight tracking-wide md:text-lg lg:text-xl xl:text-2xl">
               Free Tier
             </h6>
             <div className="max-h-32 flex-1 text-center">
@@ -42,10 +69,12 @@ const Pricing: React.FC<PricingProps> = () => {
             <h5 className="my-4 text-center font-comfortaa text-base md:text-lg lg:text-xl xl:text-2xl">
               15 Campaigns Daily
             </h5>
-            <button className="btn-block btn m-2 text-xl">Register Now</button>
+            <button className="btn-block btn my-2 text-xl">
+              Download Or Use The Web Version For Free
+            </button>
           </div>
-          <div className="card-bordered card flex flex-col items-center justify-between rounded-2xl border-2 border-leadistroWhite py-5">
-            <h6 className="text-base md:text-lg lg:text-xl xl:text-2xl">
+          <div className="card-bordered card card-body flex flex-col items-center justify-between rounded-2xl border-2 border-leadistroWhite py-5 font-poppins">
+            <h6 className=" card-title text-base font-extralight tracking-wide md:text-lg lg:text-xl xl:text-2xl">
               Pro Tier
             </h6>
             <div className="max-h-32 flex-1 text-center">
@@ -68,7 +97,9 @@ const Pricing: React.FC<PricingProps> = () => {
             <h5 className="my-4 text-center font-comfortaa text-base md:text-lg lg:text-xl xl:text-2xl">
               15 Campaigns Daily
             </h5>
-            <button className="btn-block btn my-2 text-xl">Register Now</button>
+            <button className="btn-block  btn my-2 text-xl">
+              Register Now
+            </button>
           </div>
         </div>
       </div>
